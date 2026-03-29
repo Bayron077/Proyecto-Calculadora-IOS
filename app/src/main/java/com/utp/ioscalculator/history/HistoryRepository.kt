3e4096a41ec93ecded1af3a5c1752fe602c26c06
Bayron <bayronc193@gmail.com>
2026-03-29 17:12:41 -0500

    se acaba de implementar la funcion de ver el historial

--- app/src/main/java/com/utp/ioscalculator/history/HistoryRepository.kt
+++ app/src/main/java/com/utp/ioscalculator/history/HistoryRepository.kt
@@ -2,9 +2,8 @@ package com.utp.ioscalculator.history
 
 import androidx.compose.runtime.mutableStateListOf
 
-// Este objeto compartirá los datos entre tu CalculatorViewModel y el HistoryViewModel de Bayron
+
 object HistoryRepository {
-    // Lista reactiva para que Compose se actualice en tiempo real
     val operationsList = mutableStateListOf<String>()
 
     fun addOperation(operation: String) {
